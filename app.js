16a8b48798dae8c55af42244173eef9ac32a2874
Paul Thomas <paulthomas2107@gmail.com>
2023-03-06 09:35:44 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+console.log('Hello');
+
+console.log('Hi');
+
+function sayHi() {
+  console.log('Hello Paul');
+}
+
+const sayHello = (who, where) => {
+  console.log('Hi ' + who + ' how is ' + where);
+};
+
+sayHello('Paul', 'Manchester');
+sayHi();
